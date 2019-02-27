23c443df6ae887af805279f4823877f9ffefa32e
axbarsan <mnlk123@yahoo.com>
2019-02-28 01:32:37 +0200

    Show sheet dialogs on macos

--- src/renderer/image-process/controllers/ImageDialogController.ts
+++ src/renderer/image-process/controllers/ImageDialogController.ts
@@ -4,46 +4,35 @@ export default class ImageDialogController {
   protected static isOpen: boolean = false;
 
   public static showParentFolderDialog(cb: DialogCallback): void {
-    if (ImageDialogController.isOpen)
-      return;
-
-    const { dialog } = require("electron").remote;
-
-    ImageDialogController.isOpen = true;
-
-    dialog.showOpenDialog({
+    ImageDialogController.openDialogWithSettings({
       title: "Pick a folder with images in it",
       buttonLabel: "Select",
       properties: [
         "openDirectory"
-      ],
-    }, (folderPath: string[] | undefined): void => {
-      const pathToSend: string | null =
-        (folderPath !== undefined
-          ? folderPath[0]
-          : null);
-
-      cb(pathToSend);
-      ImageDialogController.isOpen = false;
-    });
+      ]
+    }, cb);
   }
 
   public static showTargetFolderDialog(cb: DialogCallback): void {
-    if (ImageDialogController.isOpen)
-      return;
-
-    const { dialog } = require("electron").remote;
-
-    ImageDialogController.isOpen = true;
-
-    dialog.showOpenDialog({
+    ImageDialogController.openDialogWithSettings({
       title: "Pick a folder where to export the processed files",
       buttonLabel: "Select",
       properties: [
         "openDirectory",
         "createDirectory"
       ],
-    }, (folderPath: string[] | undefined): void => {
+    }, cb);
+  }
+
+  protected static openDialogWithSettings(settings: Electron.OpenDialogOptions, cb: DialogCallback): void {
+    if (ImageDialogController.isOpen)
+      return;
+
+    const { dialog, getCurrentWindow } = require("electron").remote;
+
+    ImageDialogController.isOpen = true;
+
+    dialog.showOpenDialog(getCurrentWindow(), settings, (folderPath: string[] | undefined): void => {
       const pathToSend: string | null =
         (folderPath !== undefined
           ? folderPath[0]
