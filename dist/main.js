6a45595d6895c8b7bc66c90a2d603fd10508595e
axbarsan <mnlk123@yahoo.com>
2019-02-28 01:24:05 +0200

    Remove dist folder from git

--- dist/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var main_1 = require("./main/");
-exports.application = new main_1.default();
-//# sourceMappingURL=main.js.map
