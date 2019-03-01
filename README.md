13b3c8b02695a4b37a997920164b9c17e9d4d77f
axbarsan <mnlk123@yahoo.com>
2019-03-01 16:29:51 +0200

    Update readme and licensing

---
+++ README.md
@@ -0,0 +1,7 @@
+# ottercompress
+
+> An otter compressing some JPEGs and PNGs :P
+
+## License
+
+MIT © [axbarsan](https://github.com/axbarsan)
--- src/main/index.ts
+++ src/main/index.ts
@@ -12,7 +12,7 @@ export default class Application {
     Application.browserWindow = browserWindow;
 
     Application.app.setAboutPanelOptions({
-      copyright: "Copyright (C) 2019 axbarsan. All rights reserved."
+      copyright: "MIT (C) 2019 axbarsan. All rights reserved."
     });
 
     Application.app.on("ready", Application.onReady);
